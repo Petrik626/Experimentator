4f620ced2cffa71d98d46d9fb9598c6125471d03
Ilya Petrov <petrik626@gmail.com>
2018-04-29 15:26:48 +0300

    The model has been updated. Connection to the database has been configured. Infrastructure ASP .Net Core Identity has been added.

---
+++ Experimentator/Models/ExperimentatorDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Experimentator.Models
+{
+    class ExperimentatorDbContext:IdentityDbContext<ExperimentatorUser>
+    {
+        public ExperimentatorDbContext(DbContextOptions<ExperimentatorDbContext> options) : base(options) { }
+    }
+}
